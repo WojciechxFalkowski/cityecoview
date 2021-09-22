2833b82ffe730b5d4fb2bd069d186079e3813c09
Wojciech Falkowski <wojciech.falkowski@movecloser.pl>
2021-09-22 23:56:07 +0200

    Updates

--- nuxt-folder/static/1632347660/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcityecoview\u002Fnuxt-folder\u002Fstatic\u002F1632347660",layout:"default",error:a,state:{var1:a,var2:a},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fcityecoview\u002F",assetsPath:"\u002Fcityecoview\u002Fnuxt-folder\u002F",cdnURL:a}}}}(null));
