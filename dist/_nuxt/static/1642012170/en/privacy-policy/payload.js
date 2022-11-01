30e6701743a6409a4e4619d5c5d012b688c18249
Wojciech Falkowski <wojciech.falkowski@movecloser.pl>
2022-11-01 15:51:36 +0100

    Add dist

--- dist/_nuxt/static/1642012170/en/privacy-policy/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/en/privacy-policy", {data:[{}],fetch:{},mutations:[]});
--- dist/_nuxt/static/1642012170/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fprivacy-policy","\u002F","\u002Fdata-deletion-instructions","\u002Fen\u002Fdata-deletion-instructions","\u002Fen","\u002Fen\u002Fprivacy-policy"]})
